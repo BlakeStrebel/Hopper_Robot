13188cc65f99813508fa3a8853d3cd02c4fb3418
BlakeStrebel <blakestrebel2018@u.northwestern.edu>
2016-09-21 17:03:14 -0500

    Various updates

--- read_plot_matrix_force.m
+++ read_plot_matrix_force.m
@@ -12,7 +12,7 @@ end
 for i =1:nsamples
     data(i,1) = data(i,1)/1000;                     % convert um -> mm
     data(i,2) = data(i,2)*12.5;                  % convert A -> N
-    data(i,3) = data(i,3)*145/512;          % convert counts -> N
+    data(i,3) = data(i,3)*145/512;               % convert counts -> N
     data(i,4) = ref(i)*145/512;
     times(i) = (i-1)*0.5;                       % 0.5 ms between samples
 end
@@ -21,19 +21,19 @@ if nsamples > 1
     if graph == 1
     figure
 
-    stairs(times,data(:,1:4));            % plot the reference and actual
+    stairs(times,data(:,2:4));            % plot the reference and actual
     % compute the average error
     score = mean(abs(data(:,4)-data(:,3)));
     title(sprintf('Avg error: %5.2f N',score))
-    ylabel('Position (mm)')
+    ylabel('Force (N)')
     xlabel('Time (ms)')
-    legend('position','motor current (N)','ATI force (N)', 'ref force (N)')
+    legend('Motor current','ATI force', 'ref force')
     
-%     figure
-%     plot(data(:,2),data(:,3));
-%     title('Current vs Position');
-%     xlabel('Position (mm)');
-%     ylabel('Current (A)');
+    figure
+    plot(data(:,1),data(:,3));
+    title('Force vs Position');
+    xlabel('Position (mm)');
+    ylabel('Force (N)');
     end
 else
     fprintf('Only 1 sample received\n')
