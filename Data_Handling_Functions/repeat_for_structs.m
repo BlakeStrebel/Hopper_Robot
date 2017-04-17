333f717d5700d298bbff9ef72df6ee88ae0aa0ca
BlakeStrebel <blakestrebel2018@u.northwestern.edu>
2017-04-17 14:47:31 -0500

    Data Handling

--- Data_Handling_Functions/repeat_for_structs.m
+++ Data_Handling_Functions/repeat_for_structs.m
@@ -7,25 +7,18 @@ function repeat_for_structs(list)
 % Example list: 
 %   list = {avg_max,avg_s0,avg_s5,avg_s10,avg_s15,avg_s20,avg_s25,avg_s30,avg_s40,avg_s50,avg_s60,avg_s75,avg_s100};
 
-figure;
-hold on;
 
 i = 1;
 for structs = list
    % Modify this region %
-   if i == 1
-       plot(mean(structs{1}.pos(:,1:3),2),mean(structs{1}.force(:,1:3),2));
-   else
-       plot(mean(structs{1}.pos(:,4:6),2),mean(structs{1}.force(:,4:6),2));
-   end
-
+   figure; 
+   plot(structs{1}.pos(:,1),structs{1}.force(:,1));
    i = i + 1;
 end
 
 
-title('Force vs Position')
- legend('max','0','5','10','15','20','25','30','40','50','60','75','100')
-xlabel('Position (mm)')
-ylabel('Force (N)')
+% title('Force vs Position')
+% xlabel('Position (mm)')
+% ylabel('Force (N)')
 
 hold off;
