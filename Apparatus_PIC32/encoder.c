a2e5606e6bef6ab30fe01b5573531a4f9029661b
BlakeStrebel <blakestrebel2018@u.northwestern.edu>
2017-07-21 15:21:01 -0500

    fixed CS pin assignment

--- Apparatus_PIC32/encoder.c
+++ Apparatus_PIC32/encoder.c
@@ -1,5 +1,5 @@
 #include "NU32.h"
-#include "encoder.h"                   
+#include "encoder.h"
 
 #define CS LATDbits.LATD0	// chip select pin for encoder
 
@@ -9,7 +9,7 @@ static int encoder_command(int read) { // send a command to the encoder chip
   SPI3BUF = read;                      // send the command
   while (!SPI3STATbits.SPIRBF) { ; }   // wait for the response
   SPI3BUF;                             // garbage was transferred, ignore it
-  SPI3BUF = 5;                         // write garbage, but the read will have the data
+  SPI3BUF = 9;                         // write garbage, but the read will have the data
   while (!SPI3STATbits.SPIRBF) { ; }
   CS = 1;							   // stop data transmission
   return SPI3BUF;
@@ -20,7 +20,7 @@ int encoder_counts(void) {
 }
 
 int encoder_position(void) {
-	static int position; 
+	static int position;
 	position = (32768 - encoder_counts())*10;	// convert counts to position (um)
     return position;
 }
@@ -30,18 +30,18 @@ void encoder_reset(void) {
 }
 
 void encoder_init(void) {
-  
-  TRISDbits.TRISD4 = 0;	// setup chip select pin as output
+
+  TRISDbits.TRISD0 = 0;	// setup chip select pin as output
   CS = 1;
-  
+
   // SPI initialization for reading from the decoder chip
   SPI3CON = 0;              // stop and reset SPI3
   SPI3BUF;                  // read to clear the rx receive buffer
-  SPI3BRG = 0x3;            // bit rate to 20 MHz, SPI4BRG = 80000000/(2*desired)-1
+  SPI3BRG = 3999;            // bit rate to 20 MHz, SPI4BRG = 80000000/(2*desired)-1
   SPI3STATbits.SPIROV = 0;  // clear the overflow
   SPI3CONbits.MSTEN = 1;    // master mode
   SPI3CONbits.MODE16 = 1;   // 16 bit mode
-  SPI3CONbits.MODE32 = 0; 
+  SPI3CONbits.MODE32 = 0;
   SPI3CONbits.SMP = 1;      // sample at the end of the clock
   SPI3CONbits.ON = 1;       // turn SPI3 on
 }
